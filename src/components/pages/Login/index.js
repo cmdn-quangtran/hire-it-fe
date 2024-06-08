1321d5a41a301c7c2f19c133ff15ec20ba5d26ff
cmvn-quangtran <quang.tran@classmethod.vn>
2024-06-08 09:07:10 +0700

    change env

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -1,19 +1,7 @@
 import React, { useEffect } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import CssBaseline from "@mui/material/CssBaseline";
-import {
-  Button,
-  Grid,
-  Paper,
-  Box,
-  Avatar,
-  TextField,
-  Typography,
-  Checkbox,
-  FormControlLabel,
-} from "@mui/material";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { Form, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -24,17 +12,13 @@ import {
 } from "../../../store/AuthSlice";
 import { useNavigate } from "react-router-dom";
 import SpinnerLoading from "../../commons/SpinnerLoading";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-const defaultTheme = createTheme();
-
 function Login() {
   const loading = useSelector(selectIsLoading);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleSubmit = async (e) => {
+  const handle_submit = async (e) => {
     e.preventDefault();
     const actionResult = await dispatch(login({ email, password }));
     if (login.fulfilled.match(actionResult)) {
@@ -62,97 +46,68 @@ function Login() {
     document.title = "Login | Hire IT";
   }, []);
   return !account ? (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage:
-              "url(https://static.vecteezy.com/system/resources/previews/005/879/539/non_2x/cloud-computing-modern-flat-concept-for-web-banner-design-man-enters-password-and-login-to-access-cloud-storage-for-uploading-and-processing-files-illustration-with-isolated-people-scene-free-vector.jpg  )",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                name="email"
-                placeholder="Enter your email"
-                value={email}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                name="password"
-                placeholder="Enter your password"
-                value={password}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+    <div>
+      <div className="login-page">
+        <div className="login-container">
+          <div className="form">
+            <Form onSubmit={handle_submit} className="login-form">
+              <h1>Login</h1>
+              <Form.Group
+                as={Row}
+                className="mb-3"
+                controlId="formHorizontalEmail"
               >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link className="signup-link" to="/register">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+                <Form.Label column sm={2}>
+                  Email
+                </Form.Label>
+                <Col sm={20}>
+                  <Form.Control
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    name="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    required
+                  />
+                </Col>
+              </Form.Group>
+
+              <Form.Group
+                as={Row}
+                className="mb-3"
+                controlId="formHorizontalPassword"
+              >
+                <Form.Label column sm={2}>
+                  Password
+                </Form.Label>
+                <Col sm={20}>
+                  <Form.Control
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    name="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    required
+                  />
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row} className="mb-1">
+                <Col sm={{ span: 10, offset: 1 }}>
+                  <Button type="submit">Sign in</Button>
+                </Col>
+              </Form.Group>
+              <p>
+                {" "}
+                You haven't account?
+                <Link className="signup-link" to="/register">
+                  Sign Up
+                </Link>
+              </p>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </div>
   ) : (
     <SpinnerLoading loading={loading} />
   );
